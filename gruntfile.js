3270523f9bf613e6d764d8d1352e426f9127604c
Hancoson <hancoson@163.com>
2014-12-11 17:47:16 +0800

    vso_grunt commit

---
+++ gruntfile.js
@@ -0,0 +1,147 @@
+
+module.exports = function(grunt){
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            //files: ['src/pages/<%= grunt.config.get("page") %>/*.js'],
+            page: {
+                options: {
+                    force:true,
+                    globals: {
+                        jQuery: true,
+                        console: true,
+                        module: true
+                    }
+                },
+                src: ['src/pages/<%= grunt.config.get("page") %>/*.js']
+            }
+        },
+        copy: {
+            common: {
+                src: ['common/**/*'],
+                dest: 'build/',
+                filter: 'isFile'
+            }
+        },
+        concat: {
+            options: {
+<<<<<<< HEAD
+                //separator: ';',
+=======
+                separator: ';',
+>>>>>>> ae5a0472506ac3b816372a8e72509766a75a5ae2
+                banner: '/* \n *@name: <%= pkg.name %>\n *@author: <%= pkg.author %>\n *@url: <%= pkg.homepage %>\n *@date: <%= grunt.template.today("dd-mm-yyyy") %>\n*/\n'
+            },
+            page: {
+                 src: ['src/pages/<%= grunt.config.get("page") %>/*.js'],
+                 dest: 'build/pages/<%= grunt.config.get("page") %>/index.js'
+            }
+        },
+        css_combo: {
+            options: {
+                banner: '/* \n *@name: <%= pkg.name %>\n *@author: <%= pkg.author %>\n *@url: <%= pkg.homepage %>\n *@date: <%= grunt.template.today("dd-mm-yyyy") %>\n*/\n'
+            },
+            page: {
+                src: 'src/pages/<%= grunt.config.get("page") %>/index.css',
+                dest: 'build/pages/<%= grunt.config.get("page") %>/index.css'
+            }
+        },
+        requirejs: {
+            page: {
+                options: {
+                    force:true,
+                    banner: '/* \n *@name: <%= pkg.name %>\n *@author: <%= pkg.author %>\n *@url: <%= pkg.homepage %>\n *@date: <%= grunt.template.today("dd-mm-yyyy") %>\n*/\n',
+                    baseUrl: 'src/pages/<%= grunt.config.get("page") %>',
+                    mainConfigFile: 'common/config.js',
+                    src:['**/*.js','!jquery.min.js'],
+                    //name: '<%= grunt.config.get("page") %>',
+                    name:'index',
+                    out: 'build/pages/<%= grunt.config.get("page") %>/index.js'
+                }
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/* \n *@name: <%= pkg.name %>\n *@author: <%= pkg.author %>\n *@url: <%= pkg.homepage %>\n *@date: <%= grunt.template.today("dd-mm-yyyy") %>\n*/\n'
+            },
+            page: {
+                src:'build/pages/<%= grunt.config.get("page") %>/index.js',
+                dest: 'build/pages/<%= grunt.config.get("page") %>/index.min.js'
+            },
+            common: {
+                expand:true,
+                cwd:'common/',
+                src:['**/*.js','!**/*.min.js'],
+                dest:'build/common',
+                ext:'.min.js'
+            }
+        },
+        cssmin: {
+            options: {
+                banner: '/* \n *@name: <%= pkg.name %>\n *@author: <%= pkg.author %>\n *@url: <%= pkg.homepage %>\n *@date: <%= grunt.template.today("dd-mm-yyyy") %>\n*/'
+            },
+            common: {
+                expand:true,
+                cwd:'common/',
+                src:'**/*.css',
+                dest:'build/common/',
+                ext:'.min.css'
+            },
+            page: {
+                expand:true,
+                cwd:'build/pages/<%= grunt.config.get("page") %>/',
+                src:'index.css',
+                dest:'build/pages/<%= grunt.config.get("page") %>/',
+                ext:'.min.css'
+            }
+        },
+        imagemin:{
+            common:{
+<<<<<<< HEAD
+                options: {
+                    optimizationLevel: 7, //定义 PNG 图片优化水平
+                    pngquant: true
+                },
+=======
+>>>>>>> ae5a0472506ac3b816372a8e72509766a75a5ae2
+                files:[{
+                    expand:true,
+                    cwd:'images/',
+                    src:['**/*.{png,jpg,gif}'],
+                    dest: 'build/images/'
+                }]
+            }
+<<<<<<< HEAD
+
+=======
+>>>>>>> ae5a0472506ac3b816372a8e72509766a75a5ae2
+        }
+    });
+
+    /**
+     * 载入使用到的通过NPM安装的模块
+     */
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    /**
+     * 注册基本任务
+     */
+    
+    //grunt.registerTask('build',['jshint','copy:common','concat:page','css_combo:page','uglify:page','cssmin:page']);
+    grunt.registerTask('build',function(pageName){
+        if(pageName == 'common'){
+            grunt.task.run(['copy:common','cssmin:common','uglify:common']);
+<<<<<<< HEAD
+        }else if(pageName == 'images'){
+=======
+        }else if(pageName == 'image'){
+>>>>>>> ae5a0472506ac3b816372a8e72509766a75a5ae2
+            grunt.task.run(['imagemin:common']);
+        }else{
+            grunt.config.set('page',pageName);
+            //grunt.task.run(['jshint','copy:common','requirejs:page','css_combo:page','uglify:page','cssmin:page']);
+            grunt.task.run(['jshint:page','concat:page','css_combo:page','uglify:page','cssmin:page']);
+        } 
+    });
+}
